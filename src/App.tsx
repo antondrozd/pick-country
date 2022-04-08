d8af90fb748962da4a37884cc9409026cf289f47
Anton Drozd <anton.drozd.95@gmail.com>
2022-04-09 01:08:15 +0300

    feat: add bg and retry

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,22 @@ function App() {
   const [finalCountry, setFinalCountry] = useState<string | null>(null)
 
   const handleClick = () => {
-    const generatedCountry: string = getRandomCountry()
+    if (isSpinning) {
+      const generatedCountry: string = getRandomCountry()
 
-    setFinalCountry(generatedCountry)
-    setIsSpinning(false)
+      setFinalCountry(generatedCountry)
+      setIsSpinning(false)
+    } else {
+      setFinalCountry(null)
+      setIsSpinning(true)
+    }
   }
 
   return (
     <PageWrapper>
-      {/* <Wrapper> */}
       <Button isSpinning={isSpinning} onClick={handleClick}>
-        {isSpinning ? "Страну мне!" : "🔥🔥🔥"}
+        {isSpinning ? "Страну мне!" : "Phew... Again please!"}
       </Button>
-      {/* {countries &&
-          countries.map((country, index) => (
-            <Country key={index}>{country}</Country>
-          ))} */}
-      {/* </Wrapper> */}
       {finalCountry && (
         <CountryWrapper>
           <Zlata src="./zlata.webp" alt="zlata wow" />
@@ -38,7 +37,7 @@ function App() {
               "+"
             )}`}
           >
-            {finalCountry}
+            🔥{finalCountry}🔥
           </Country>
         </CountryWrapper>
       )}
@@ -83,11 +82,25 @@ const moveY = keyframes`
 `
 
 const PageWrapper = styled.main`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
+
+  ::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: url("./bg.jpeg") no-repeat center;
+    background-size: cover;
+    opacity: 0.2;
+    z-index: -1;
+  }
 `
 
 const CountryWrapper = styled.div`
@@ -97,16 +110,40 @@ const CountryWrapper = styled.div`
 `
 
 const Country = styled.a`
+  position: relative;
   font-family: "Rubik Bubbles", cursive;
   font-size: 36px;
+  text-align: center;
   text-decoration: none;
   color: #000;
+  animation: ${zlataEffect} linear 1s;
+
+  ::after {
+    content: "☝ \n click me!";
+    position: absolute;
+    bottom: -30px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100px;
+    font-size: 14px;
+    text-align: center;
+  }
+
+  :hover {
+    text-decoration: underline;
+  }
 `
 
 const Zlata = styled.img`
   width: 250px;
   margin: 40px 0;
   border-radius: 50%;
+  user-select: none;
+  animation: ${spin} linear 0.5s;
+
+  :hover {
+    animation: ${spin} linear 0.5s infinite;
+  }
 `
 
 // const Wrapper = styled.div`
@@ -155,22 +192,29 @@ const Button = styled.button<{ isSpinning: boolean }>`
   position: relative;
   width: ${BUTTON_WIDTH}px;
   padding: 20px;
+  font-family: "Shadows Into Light", cursive;
+  font-weight: 600;
   font-size: 21px;
-  background-color: #b846ef;
   color: #feeef8;
+  background-color: #b846ef;
   border: none;
   border-radius: 20px;
   cursor: pointer;
   user-select: none;
+  transition: transform 0.2s;
 
   ${(props) =>
     props.isSpinning
       ? css`
           animation: ${spin} linear 2s infinite,
             ${moveX} 2s linear 0s infinite alternate,
-            ${moveY} 3.4s linear 0s infinite alternate;
+            ${moveY} 3.5s linear 0s infinite alternate;
         `
       : css`
           animation: ${zlataEffect} linear 1s;
+
+          :hover {
+            transform: scale(1.1);
+          }
         `}
 `
